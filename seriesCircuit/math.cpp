ddfce84171bf77233937066d44b8e5e7b89d18c3
cryptosky-platform <58134242+cs-joy@users.noreply.github.com>
2022-06-26 22:46:25 +0600

    init

---
+++ seriesCircuit/math.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int loop;
+    float R[50], result(0);
+
+    cout << "How much resistance available in your circuit?";
+    cin >> loop;
+
+    cout << "Value of Resitances" << endl;
+    
+    for(int i = 1; i <= loop; i++){
+        cout << "Resistance R[" << i << "]= ";
+        cin >> R[i];
+        result += R[i];
+    }
+
+    cout << "Total Resistance of your circuit= " << result;
+
+    return 0;
+}
