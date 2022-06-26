05940f04a8718d8746a8edeae5070c588ca57ce3
cryptosky-platform <58134242+cs-joy@users.noreply.github.com>
2022-06-27 01:08:28 +0600

    Update math.cpp

--- seriesCircuit/math.cpp
+++ seriesCircuit/math.cpp
@@ -1,15 +1,38 @@
 #include <iostream>
 using namespace std;
-
+/*
 #include "parallel.hpp"
 #include "series.hpp"
+*/
 #include "optionM.hpp"
+
+void optionMenu(){
+    int like;
+    cout << "|-----------\n";
+    cout << "|-Choose one option for calculation: \n";
+    cout << "|-----------\n";
+    cout << "1. Series Circuit\n";
+    cout << "2. Parallel Circuit\n";
+    cout << "3. Exit\n\n";
+
+    cin >> like;
+    if (like == 1)
+        {
+            Series();
+        }
+        else if (like == 2)
+        {
+            Parallel();
+        }
+        else
+        {
+            exit(0);
+        }
+}
 
 int main(){
 
-    while(true){
         optionMenu();
-    }
 
     return 0;
 }
